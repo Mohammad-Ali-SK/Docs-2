33d70d1ad3b131c09c8ab437b1e36dadbf17f276
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-12-13 22:03:52 +0530

    Just_Fun

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,25 +2,30 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { CiSaveDown1 } from "react-icons/ci";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { motion } from "framer-motion"
 
 const Card = ({data}) => {
   return (
-    <div className=' w-60 h-72 rounded-[20px] bg-zinc-900 p-5 relative text-white overflow-hidden'>
+    <motion.div drag whileDrag={{scale:1.2}} className=' w-60 h-72 rounded-[20px] bg-zinc-900 p-5 relative text-white overflow-hidden'>
 
         <span> <FaRegFileAlt/> </span>
 
-        <p className=' mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid !</p>
+       
+        <p className=' mt-3'>{data.text}</p>
 
         <div className=" absolute left-0 w-full p-3 pr-5 top-[60%] text-white font-thin flex items-center justify-between">
-            <p>0.09mb</p>
-            <span className=' text-2xl'> <CiSaveDown1/> </span>
+            <p>{data.size}</p>
+            <span className=' text-2xl'>
+                {data.icon ? <IoCloseCircleOutline/> : <CiSaveDown1/> }
+            </span>
         </div>
 
 
-        <div className=" bottom-0 absolute w-full h-12 text-xl left-0 bg-green-600 flex justify-center items-center text-center"></div>
+        {data.lol ? <div className={` down bottom-0 absolute w-full h-12 text-xl left-0 ${data.color} flex justify-center items-center text-center`}>{data.download ? "Download" : "Done"}</div> : null}
 
 
-    </div>
+    </motion.div>
     
     )
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,18 +2,19 @@
 import React from 'react'
 import Card from './Card'
 
+
 const Main = () => {
     const data = [
-        {icon:true,text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia facere corporis natus excepturi .",size:".03mb", download:true},
-        {icon:true,text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia facere corporis natus excepturi .",size:".03mb", download:true},
-        {icon:true,text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia facere corporis natus excepturi .",size:".03mb", download:true},
+        {icon:false,text:"sit amet consectetur adipisicing elit. Quia facere corporis natus excepturi .",size:".03mb", download:true,color:"bg-green-600",lol:true},
+        {icon:true,text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia facere corporis natus excepturi .",size:"Close", download:false, color: 'bg-blue-600',lol:false},
+        {icon:false,text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit.  .",size:".03mb", download:true,color:"bg-blue-600",lol:true},
 
     ];
   return (
     <main className=' p-5 absolute w-full h-screen top-0 left-0 z-5 flex flex-wrap gap-3'>
      {data.map((item,index) => (
         // eslint-disable-next-line react/jsx-key
-        <Card/>
+        <Card data={item}/>
      ))}
     
 
@@ -22,4 +23,5 @@ const Main = () => {
     )
 }
 
+{/* <p className=' bg-green-600'</p> */}
 export default Main
